b1e0b1fc94fc9b0ea83534d4efb616d97d4776af
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-08-09 18:31:35 -0400

    flash added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
   def index
+    #create flash message
+    flash.now[:notice] = "Logged in Successfully. Welcome to the app!"
+    flash.now[:error] = "You are not authorized to view this page."
+    flash.now[:alert] = "Invalid email or password."
   end
 end
