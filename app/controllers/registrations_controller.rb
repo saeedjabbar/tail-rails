7adf738f07ff77123b1d63c17261fbfcd964c066
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-08-10 11:51:21 -0400

    test

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    render plain: 'thanks'
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Invalid email address' }
+end
