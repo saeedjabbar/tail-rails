15845a7ca12f5dabd949d3a8847e7c0fa3489eff
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-08-11 00:52:02 -0400

    updated pwd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
 
+  #create password routes
+  get 'password', to: 'passwords#edit', as: 'edit_password'
+  patch 'password', to: 'passwords#update'
+
   #signup routes
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
 
-  
+
   delete 'logout', to: 'sessions#destroy'
   root to: 'main#index'
 end
