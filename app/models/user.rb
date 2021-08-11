5c75e159252d886312e53b619d3943c23dfd93b5
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-08-11 12:40:55 -0400

    twitter auth added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
     has_secure_password
     validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Invalid email address' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,10 @@ Rails.application.routes.draw do
   get 'password/reset/edit', to: 'password_resets#edit'
   patch 'password/reset/edit', to: 'password_resets#update'
 
+  #create twitter omniauth routes
+  get '/auth/twitter/callback', to: 'omniauth_callbacks#twitter'
 
   delete 'logout', to: 'sessions#destroy'
+  
   root to: 'main#index'
 end
